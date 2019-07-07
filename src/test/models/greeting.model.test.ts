6c0753403ebf14cb959f0ce8b321ccd07c4f6dd7
edisonpaul4 <edisonpaul4@gmail.com>
2019-07-07 10:16:24 -0500

    Test error

--- src/test/models/greeting.model.test.ts
+++ src/test/models/greeting.model.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { GreetingResponseModel, IGreetingRequestModel } from '../../models/greeting.model';
 
 describe('Greeting Response', () => {
-    it('Accepts a faulty request and validate the body returning a valid response', () => {
+    it('Accepts a valid request and validate the body returning a valid response', () => {
         const response = new GreetingResponseModel();
         const requestMockup = {
             from: 'from',
@@ -13,13 +13,13 @@ describe('Greeting Response', () => {
         expect(response.getResponse(requestMockup).message).to.equal('Hello to your message will be send');
     });
 
-    it('Accepts a request and validate the body returning a ERROR response', () => {
+    it('Accepts a faulty request and validate the body returning a ERROR response', () => {
         const response = new GreetingResponseModel();
         const requestMockup = {
             from: 'from',
             message: 'message',
             timeToLifeSec: 90,
         } as IGreetingRequestModel;
-        expect(response.getResponse(requestMockup).message).to.equal('ERROR');
+        expect(response.getResponse(requestMockup).message).to.equal('aaa');
     });
 });
